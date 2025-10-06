42f9b70e59bc330246ca5d0f1dadb7fbfe18f591
tienhienn <huynhletienhien@gmail.com>
2025-10-06 23:22:15 +0700

    de o day

--- BTChuyenActivity/app/src/main/java/com/example/btchuyenactivity/LoginActivity.java
+++ BTChuyenActivity/app/src/main/java/com/example/btchuyenactivity/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.btchuyenactivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,47 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnDangNhap;
+    EditText edtTaiKhoan, edtMatKhau;
+
+    private final String userNameDefault = "admin";
+    private final String passwordDefault = "12345";
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        btnDangNhap = findViewById(R.id.btnDangNhap);
+        edtMatKhau = findViewById(R.id.edtPassword);
+        edtTaiKhoan = findViewById(R.id.edtTaiKhoan);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userName = edtTaiKhoan.getText().toString().trim();
+                String password = edtMatKhau.getText().toString().trim();
+
+                if(userName.equals(userNameDefault) && password.equals(passwordDefault)){
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("username", userName);
+                    bundle.putString("password",password);
+
+                    Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
+            }
         });
     }
 }
